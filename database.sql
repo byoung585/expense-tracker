65d9fdd9f3ec5f9afec325ff387317a12ff7792c
Ben Young <bay19@pitt.edu>
2021-05-30 00:37:19 -0400

    record date when each entry was added

--- database.sql
+++ database.sql
@@ -3,6 +3,21 @@ DROP TABLE IF EXISTS Expenses;
 CREATE TABLE Expenses (
     id SERIAL PRIMARY KEY,
     amount numeric,
-    category varchar(64) CHECK(category IN ('rent', 'groceries', 'food', 'entertaintment', 'misc')),
-    comment varchar(128)
+    category varchar(64) CHECK(category IN ('rent', 'groceries', 'food', 'entertainment', 'misc')),
+    comment varchar(128),
+    date_added date
 );
+
+CREATE OR REPLACE FUNCTION set_date()
+RETURNS trigger AS
+$$
+BEGIN
+    NEW.date_added = current_date;
+    RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER set_date_trigger
+BEFORE INSERT ON Expenses
+FOR EACH ROW
+EXECUTE FUNCTION set_date();
